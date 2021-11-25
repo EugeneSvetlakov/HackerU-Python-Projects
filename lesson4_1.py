1fa692fafde6a42850970b729e77ae7601860b39
Светлаков Евгений Сергеевич <svetlakov_es@rodina.loc>
2021-11-25 11:50:08 +0300

    some optimisation

--- lesson4_1.py
+++ lesson4_1.py
@@ -2,7 +2,7 @@ import os
 
 
 # Работа с файлами
-# Чтение файла в текстовом режиме
+# region Чтение файла в текстовом режиме
 file_name = "lesson4.txt"
 if os.path.isfile(file_name):
     with open(file_name, encoding="utf-8", mode="rt") as read_file:
@@ -24,20 +24,23 @@ if os.path.isfile(file_name):
         # read_file_lines = file.readlines(10)
         for line in read_file:
             print(line)
+# endregion
 
-# Запись в файл
+# region Запись в файл
 with open("lesson4_write.txt", encoding="utf-8", mode="w") as write_file:
     write_file.write("one\ntwo\nthree")
     # file.writelines()
+# endregion
 
-# Чтение файла в бинарном режиме
+# region Чтение файла в бинарном режиме
 if os.path.isfile(file_name):
     with open(file_name, mode="rb") as read_file:
         read_bites = read_file.read()
         bites_to_text = read_bites.decode('cp1256')
         print(bites_to_text)
+# endregion
 
-# Запись в финарном режиме
+# region Запись в финарном режиме
 # Реализовано не совсем верно вроде, стоит почитать про
 # конвертауию текста в бинарный код и запись кода в файл
 # за одно потренироваться чтению и декодированию бинарника
@@ -45,11 +48,13 @@ file_name = "lesson4_wr_bite.bit"
 with open(file_name, mode="wb") as write_file:
     bytes_example = bytes("one two", 'utf-8')
     write_file.write(bytes_example)
+# endregion
 
-# Комбинированные режимы
+# region Комбинированные режимы
 # Open_to_read_and_can_write = open("file.txt", "r+")
 # Open_to_read_and_can_write.close()
 # Open_to_read_and_can_write = open("file.txt", "r+")
 # Open_to_read_and_can_write.close()
 # Open_to_read_and_can_write_to_end = open("file.txt", "w+")
 # Open_to_read_and_can_write_to_end.close()
+# endregion
